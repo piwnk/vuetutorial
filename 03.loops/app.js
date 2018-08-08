a71e7d23a7690056cdc6b1aefd6f614ff8835a40
piwnk <piotr.iwaniuk@gmail.com>
2018-08-08 23:15:26 +0200

    03.assignment

---
+++ 03.loops/app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: [
+      'meat',
+      'fruit',
+      'cookies'
+    ],
+    people: [
+      {
+        name: 'Peter',
+        age: 35,
+      },
+      {
+        name: 'Emi',
+        age: 27,
+      }
+    ]
+  }
+})
